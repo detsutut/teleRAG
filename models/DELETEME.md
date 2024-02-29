6024e75a557b68a76ef135868142f5995e46883b
detsutut <buonocore.tms@gmail.com>
2024-02-29 16:23:01 +0100

    initial commit with placeholders for empty dirs

---
+++ models/DELETEME.md
@@ -0,0 +1,5 @@
+# Delete this file after fork
+
+This file is just a placeholder to push empty directories.
+
+You can safely delete this file after initialization.
---
+++ pipelines/default.py
@@ -0,0 +1,7 @@
+from scripts.simplescript import get_time
+
+def main():
+    print("Hello World!\nToday's date is " + get_time())
+
+if __name__ == "__main__":
+    main()
