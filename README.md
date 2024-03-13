81c4cb72c80f8fcd07aa45705f5902d4bc830c6d
detsutut <buonocore.tms@gmail.com>
2024-03-13 10:35:26 +0100

    README updated

--- README.md
+++ README.md
@@ -1,3 +1,70 @@
-# TeleRAG
+# TelegramBot
 
-RAG-powered telegram bots! 
+A Python Telegram bot powered with LLM and RAG for interactive messaging and conversation management.
+
+## Overview
+
+This Telegram bot utilizes natural language processing (NLP) techniques to provide interactive messaging functionalities. It integrates with a Large Language Model (LLM) for generating replies and RAG through semantic search to trigger specific actions/routines.
+
+## Features
+
+- **Interactive Messaging**: Realistic conversation through LLM powered answers.
+- **Semantic Search**: RAG with semantic search is used to trigger specific routines, commands, actions. 
+- **Conversation History**: Maintains conversation history to generate context-and-history-aware responses.
+- **Broadcast Messages**: Notify users with broadcast messages upon bot start and stop events.
+
+## Setup
+
+### Prerequisites
+
+- Python 3.x
+- A Telegram Bot already instantiated via ```BotFather```
+- Telegram API token
+
+### Installation
+
+1. Clone the repository:
+
+   ```
+   git clone https://github.com/your/repository.git
+   ```
+
+2. Install dependencies:
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+3. Set up Telegram API token:
+
+   - Obtain your Telegram API token and copy it
+   - Create a file called ```API_token``` in ```./bot```
+   - Paste your token in the API_token file (n.b. don't share this file!)
+
+4. Set up the chat database
+
+   - Rename ```./data/empty_chats.db``` to ```./data/chats.db```'
+
+### Usage
+
+1. Run the bot, passing the path to the API token:
+
+   ```
+   python TelegramBot.py /.../.../bot/API_token
+   ```
+
+2. Open Telegram, search for your bot name and start chatting
+
+## Usage Guide
+
+1. Start the conversation with the bot by sending a message.
+2. The bot will respond based on the message content, utilizing semantic search or the LLM for generating replies.
+3. Use commands such as `/config` to retrieve the bot's configuration or `/history` to view conversation history.
+
+## Contributors
+
+-  **Repository maintainer**: Tommaso M. Buonocore  [![LinkedIn][linkedin-shield]][linkedin-url]  
+
+## License
+
+This project is licensed under the [CC BY-NC-ND 4.0 License](LICENSE.md).
--- bot/telegrambot.py
+++ bot/telegrambot.py
@@ -2,6 +2,8 @@ import logging
 import os
 import sys
 
+import pandas as pd
+
 sys.path.append("/home/tommaso/Repositories/teleRAG/")
 
 from bot.botutils import check_length, split_text, load_api_token
@@ -21,7 +23,46 @@ logging.basicConfig(
 
 
 class TelegramBot:
+    """
+    A class used to represent an Animal
+
+    ...
+
+    Attributes
+    ----------
+    llm : LLM
+        the large language model that is used to generate replies
+    embedder : str
+        a small language model used for semantic search for RAG
+    actions : DataFrame
+        special routines that are triggered through semantic search
+    MAX_LEN : int
+        maximum message length in characters
+    API_TOKEN: str
+        the api token of the telegram bot
+    ACTIONS_THRESHOLD: float
+        consider semantic search result only if similarity above this threshold
+    ONSTART_MSG: str
+        the message broadcasted when the bot goes online
+    ONSTOP_MSG: str
+        the message broadcasted when the bot goes offline
+    WELCOME_MSG: str
+        the greet message sent when conversation starts for the first time
+    WELCOME_MSG: str
+        the message sent when conversation is restarted
+
+    Methods
+    -------
+    says(sound=None)
+        Prints the animals name and what sound it makes
+    """
     def __init__(self, api_token_path: str):
+        """
+        Parameters
+        ----------
+        api_token_path : str
+            path where the telegram bot API is located
+        """
         self.llm = LLM()
         self.embedder = SentenceEmbedder()
         self.actions = retrieve_actions()
@@ -30,16 +71,37 @@ class TelegramBot:
         self.ACTIONS_THRESHOLD = 0.6
         self.ONSTART_MSG = "Back online! Let meow know if you need assistance 🐱"
         self.ONSTOP_MSG = "Meowtenance time! Need to recharge and get my fur fluffed. Sweet dreams, humans! I'll be back online soon. Meanwhile, I will just ignore you 🐱"
+        self.WELCOME_MSG = "Nice to meet you! I am RagBot, meow! How can I assist you today?"
+        self.RESTART_MSG = "Memory wiped out! Meow! How can I assist you today?"
 
-    def increase_decrease_menu(self, action_id: str):
+    def increase_decrease_menu(self, action_id: str) -> InlineKeyboardMarkup:
+        """Defines an increase/decrease template for actions.
+
+        The argument 'action_id' allows to keep track of what action is being performed when the user interacts with the menu.
+        Callback data includes the action id and increase/decrease modifier
+
+        Parameters
+        ----------
+        action_id : str
+            The id of the action to perform
+        """
         keyboard = [
             [InlineKeyboardButton("-", callback_data='{"action_id": ' + action_id + ', "action": "decrease"}'),
              InlineKeyboardButton("+", callback_data='{"action_id": ' + action_id + ', "action": "increase"}'), ],
         ]
         return InlineKeyboardMarkup(keyboard)
 
-    async def button(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        """Parses the CallbackQuery and updates the message text."""
+    async def button(self, update: Update) -> None:
+        """Parses the CallbackQuery and updates the message text.
+
+        Retrieve callback data after user's interaction with 'InlineKeyboardMarkup'.
+        Increase/decrease by 25% the LLM attribute associated with the action_id in callback data.
+
+        Parameters
+        ----------
+        update : Update
+            The incoming update
+        """
         query = update.callback_query
         data = eval(query.data)
         action_name = self.actions[self.actions['id'] == data['action_id']]['name'].iloc[0]
@@ -58,7 +120,16 @@ class TelegramBot:
         await query.answer()
         await query.edit_message_text(text=f"Selected option: {data['action']}")
 
-    async def config(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def config(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send the llm configuration.
+
+        Parameters
+        ----------
+        update : Update
+            The incoming update
+        context: ContextTypes.DEFAULT_TYPE
+            The current context, reporting application that called this function, chat id and user id
+        """
         chat_id = update.effective_chat.id
         details = "Bot Configuration\n\n" + str(self.llm.gen_config)
         if check_length(details):
@@ -67,7 +138,19 @@ class TelegramBot:
                 await context.bot.send_message(chat_id=chat_id, text=chunk)
         await context.bot.send_message(chat_id=chat_id, text=details)
 
-    async def history(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def history(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send the chat history.
+
+        Privacy protected: hijacking the incoming update with a different chat_id has no effect since the chat history would be forwarded to
+        that chat_id.
+
+        Parameters
+        ----------
+        update : Update
+            The incoming update
+        context: ContextTypes.DEFAULT_TYPE
+            The current context, reporting application that called this function, chat id and user id
+        """
         chat_id = update.effective_chat.id
         details = "Conversation History\n\n" + str(get_chat(str(chat_id)))
         if check_length(details):
@@ -77,14 +160,41 @@ class TelegramBot:
         await context.bot.send_message(chat_id=chat_id, text=details)
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Nice to meet you! I am RagBot, meow! How can I assist you today?")
+        """Message sent when conversation starts for the first time"""
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=self.WELCOME_MSG)
 
     async def restart(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        """Restart the conversation.
+
+        The conversation history for this chat is erased from the database and restarted with default conversation template.
+
+        Parameters
+        ----------
+        update : Update
+            The incoming update
+        context: ContextTypes.DEFAULT_TYPE
+            The current context, reporting application that called this function, chat id and user id
+        """
         chat_id = update.effective_chat.id
         put_chat(str(chat_id), get_chat("DEFAULT"))
-        await context.bot.send_message(chat_id=chat_id, text="Memory wiped out! Meow! How can I assist you today?")
+        await context.bot.send_message(chat_id=chat_id, text=self.RESTART_MSG)
 
-    def vector_db_search(self, input_text):
+    def vector_db_search(self, input_text) -> dict | None:
+        """Semantic search in the vector database.
+
+        The incoming text is embedded and compared to the entries (i.e., embeddings) of the vector database.
+        If the similarity is high enough, the entry with the highest similarity is returned
+
+        Parameters
+        ----------
+        input_text : str
+            The input text used to search the vector database
+
+        Returns
+        -------
+        action: dict | None
+            The action selected from the vector database, together with its similarity score
+        """
         embedded_reply = self.embedder.encode(input_text)
         value, index = self.embedder.semantic_search(embedded_reply, list(self.actions["embedding"]))
         if value >= self.ACTIONS_THRESHOLD:
@@ -94,27 +204,99 @@ class TelegramBot:
         else:
             return None
 
-    def update_and_generate(self, chat_id, input_text):
+    def update_and_generate(self, chat_id, input_text) -> str:
+        """Use the LLM to generate an answer for the user and update the conversation history .
+
+        First, the chat history is retrieved from the history database.
+        Then, the conversation history, i.e., chat template, is passed to the LLM together with the input text to generate a context and history aware answer.
+        Finally, the conversation history is updated with the input_text and the answer generated.
+
+        Parameters
+        ----------
+        chat_id : int
+            The chat id of the user (n.b. Telegram ids are integers, not strings!)
+        input_text: str
+            The text
+
+        Returns
+        -------
+        answer: str
+            The answer generated with LLM
+        """
         chat_template = get_chat(str(chat_id))
         answer = self.llm.reply(user_message=input_text, chat_template=chat_template)
         logging.info(f"message generated for {chat_id}")
         put_chat(str(chat_id), answer["chat_template"])
         return answer["text"]
 
-    async def post_stop(self, application: Application) -> None:
+    async def post_stop(self, application: Application, recent=True) -> None:
+        """Callback called when the bot is stopped.
+
+        When the bot is stopped, send a broadcast message to notify users that the bot will be offline.
+        The notification can be sent to the users that interacts with the bot recently (default) or to all the users in memory
+
+        Parameters
+        ----------
+        application : Application
+            The chat id of the user (n.b. Telegram ids are integers, not strings!)
+        recent: bool
+            Whether to notify only the users that interacts with the bot recently
+
+        Returns
+        -------
+        answer: str
+            The answer generated with LLM
+        """
         logging.info(f"broadcasting message...")
-        chat_ids = get_all_chat_ids(recent=True)
+        chat_ids = get_all_chat_ids(recent=recent)
         for chat_id in chat_ids:
             await application.bot.send_message(chat_id=chat_id, text=self.ONSTOP_MSG)
 
-    async def post_init(self, application: Application) -> None:
+    async def post_init(self, application: Application, recent=True) -> None:
+        """Callback called when the bot starts.
+
+        When the bot starts, send a broadcast message to notify users that the bot is online again.
+        The notification can be sent to the users that interacts with the bot recently (default) or to all the users in memory
+
+        Parameters
+        ----------
+        application : Application
+            The chat id of the user (n.b. Telegram ids are integers, not strings!)
+        recent: bool
+            Whether to notify only the users that interacts with the bot recently
+
+        Returns
+        -------
+        answer: str
+            The answer generated with LLM
+        """
         logging.info(f"broadcasting message...")
-        chat_ids = get_all_chat_ids(recent=True)
+        chat_ids = get_all_chat_ids(recent=recent)
         for chat_id in chat_ids:
             await application.bot.send_message(chat_id=chat_id, text=self.ONSTART_MSG)
 
-
     async def reply(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        """Callback called when there is a new incoming message.
+
+        When a new message is received, the bot:
+        1) Updates session info
+        2) Sets its status to 'typing...' (only lasts for 5 seconds and there's no way to modify it)
+        3) Semantic search to get the most plausible action from the vector db
+        4A) If semantic search finds an action that is plausible enough, the correspondent routine starts
+        4B) If semantic search does not return anything, the user message is passed to the LLM and the generated answer is sent back
+
+        Parameters
+        ----------
+        update : Update
+            The incoming update
+        context: ContextTypes.DEFAULT_TYPE
+            The current context, reporting application that called this function, chat id and user id
+
+        Returns
+        -------
+        answer: str
+            The answer generated with LLM
+        """
         chat_id = update.effective_chat.id
         logging.info(f"incoming message from {chat_id}")
         update_session(str(chat_id), "TEST001")
@@ -130,6 +312,12 @@ class TelegramBot:
             await context.bot.send_message(chat_id=chat_id, text=answer)
 
     def run(self):
+        """Run the telegram bot.
+
+        Application built through ApplicationBuilder passing the API token and the init and stop callbacks.
+        Then, handlers managing various types of user-bot interaction are added to the application.
+        Finally, the app starts through the convenience method 'run_polling' which initialize, update and gracefully stop the bot.
+        """
         application = ApplicationBuilder().token(self.API_TOKEN).post_init(self.post_init).post_stop(self.post_stop).build()
         handlers = [CommandHandler("start", self.start),
                     CommandHandler('restart', self.restart),
